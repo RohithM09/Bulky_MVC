1c0d42733e287560aac99eb1ea55b3cf95490b2a
RohithM09 <rohithmidde21@gmail.com>
2025-11-28 09:59:46 +0530

    add razor pages

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -39,9 +39,69 @@ namespace BulkyWeb.Controllers
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
-
+                TempData["success"] = "Category created successfully";
                 return RedirectToAction("Index", "Category");
             }
+            return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            { return NotFound();
+            }
+            Category? obj = _db.Categories.Find(id);
+            //Category? obj1 = _db.Categories.FirstOrDefault(u=> u.Id == id);
+            //Category? obj2 = _db.Categories.Where(u => u.Id == id).FirstOrDefault();
+            if (obj==null)
+            {
+                return NotFound();
+            } 
+            return View(obj);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "category updated successfully";
+                return RedirectToAction("Index", "Category");
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? obj = _db.Categories.Find(id);
+            //Category? obj1 = _db.Categories.FirstOrDefault(u=> u.Id == id);
+            //Category? obj2 = _db.Categories.Where(u => u.Id == id).FirstOrDefault();
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+        [HttpPost,ActionName("Delete")]
+
+        public IActionResult DeletePOST(int? id)
+        {
+            Category obj = _db.Categories.Find(id);
+            if(obj==null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "category deleted successfully";
+            return RedirectToAction("Index", "Category");
+            
             return View();
         }
     }
---
+++ BulkyWebRazor_Temp/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using BulkyWebRazor_Temp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWebRazor_Temp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(new Category { Id = 1, Name = "xyz", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "abc", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "pqr", DisplayOrder = 3 });
+        }
+    }
+}
